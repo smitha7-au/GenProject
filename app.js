6429762c011a4b19e84d26d7934a48d858b33276
Archana-15 <67139651+Archana-15@users.noreply.github.com>
2020-08-04 00:09:51 +1000

    Added new task dynamically displayed in alert

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Declare global variables
+var TaskName, TaskDesc, DueDate, AssignedTo, Status;
+
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -14,5 +17,61 @@
             }, false);
         });
     }, false);
+
 })();
-// No Comments
+
+function CaptureValues() {
+    // assigning the input values in to the domain variables.
+    TaskName = document.getElementById('txtTaskName').value;
+    TaskDesc = document.getElementById('txtTaskDec').value;
+    DueDate = document.getElementById('duedate').value;
+    AssignedTo = document.getElementById('txtTaskAssigned').value;
+    Status = document.getElementById('selectStatus').value;
+
+    // Create an object of a task now and pass the input values 
+    var NewTask = new Task(100, TaskName, TaskDesc, DueDate, AssignedTo, status);
+    NewTask.addTask();
+    // NewTask.getAllTasks();
+    // NewTask.deleteTask();
+}
+
+// java script task class object
+class Task {
+    constructor(ID, TaskName, TaskDesc, DueDate, AssignedTo, Status) {
+        this.ID = ID;
+        this.TaskName = TaskName;
+        this.TaskDesc = TaskDesc;
+        this.DueDate = DueDate;
+        this.AssignedTo = AssignedTo;
+        this.Status = Status;
+
+    }
+    getAllTasks() {
+        alert("All Tasks");
+    }
+
+    getTasksWithStatus(status) {
+        alert("All Tasks by the status");
+    }
+    addTask(Task) {
+        alert("New task added with the below values \n" + "Task name :" + TaskName + "\nTask Description:" + TaskDesc + "\nDue Date:" + DueDate + "\nAssigned To:" + AssignedTo + "\nStatus:" + Status);
+        document.getElementById("Output").innerHTML = "New task added with the below values \n" + "Task name :" + TaskName + "\nTask Description:" + TaskDesc + "\nDue Date:" + DueDate + "\nAssigned To:" + AssignedTo + "\nStatus:" + Status;
+    }
+
+    deleteTask(Task) {
+        alert("delete task added");
+    }
+
+    updateTask(taskId, status) {
+        alert("Update task added");
+    }
+
+    assignTask(taskId, assignee) {
+        alert("Assign task added");
+    }
+
+    greeting() {
+        alert('New Task added successfully.');
+    };
+
+}
