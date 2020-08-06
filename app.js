358edccf2c3d341b6e99a9762e48b3122604ff0e
Archana-15 <67139651+Archana-15@users.noreply.github.com>
2020-08-06 17:43:43 +1000

    Task5

--- app.js
+++ app.js
@@ -16,3 +16,38 @@
     }, false);
 
 })();
+class Task {
+    constructor(id, name, description, assignedTo, date, status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.assignedTo = assignedTo;
+        this.date = date;
+        this.status = status;
+    }
+}
+class TaskManager {
+    constructor() {
+        this.currentId = 1;
+        this.tasks = [];
+    }
+    getAllTasks() {
+        return this.tasks;
+    }
+    getTasksByStatus() { }
+    addTask(name, description, assignedTo, dueDate, status) {
+        const task = new Task(
+            `task${this.currentId++}`,
+            name,
+            description,
+            assignedTo,
+            dueDate,
+            status
+        );
+        //push new task onto the tasks array
+        this.tasks.push(task);
+    }
+    deleteTask(id) { }
+    updateTask(id) { }
+    assignTask(task) { }
+}
