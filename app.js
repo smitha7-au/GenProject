7f51553f33847fab34b4db5989a02542941bb3bd
Smitha T <smiib4u@gmail.com>
2020-08-07 15:52:58 +1000

    Completed Sprint2 -Task 5

--- app.js
+++ app.js
@@ -1,123 +1,155 @@
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-
-})();
-
 class Task {
-    constructor(ID, name, desc, date, assignedTo, status) {
-        this.ID = ID;
-        this.TaskName = name;
-        this.TaskDesc = desc;
-        this.DueDate = date;
-        this.AssignedTo = assignedTo;
-        this.Status = status;
+    constructor(id, name, description, 
+        assignedTo, dueDate, status) {
+            this.id = id;
+            this.name = name
+            this.description = description;
+            this.assignedTo = assignedTo;
+            this.dueDate = dueDate;
+            this.status = status;
+        }
+
+        toHtmlElement() {
+            const html = `<div>
+            <div class="card border-info mt-5">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h5 class="card-title">${this.name}</h5>
+                    </div>
+                    <div class="col-md-4">
+                        <h5 class="dueDateLabel">Due Date: ${this.dueDate}
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <label for="forTaskDescription">
+                    <dt>Description</dt>
+                </label>
+                <div class="overflow-auto border">
+                    <p class="card-text">${this.description}</p>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-6 border">
+                        <label for="forAssignedTo">
+                            <dt>Assigned To</dt>
+                        </label>
+                        <p>${this.assignedTo}</p>
+                    </div>
+                    <div class="col-md-6 border">
+                        <label for="selectStatus">
+                            <dt>Status</dt>
+                        </label>
+                        <p>${this.status}</p>
+                    </div>
+
+                </div>
+            </div>
+            <ul class="nav nav-tabs ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Update</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Delete</a>
+                </li>
+            </ul>
+            </div>`;
+            
+            // const html = `Task Id: ${this.id} <br> Task Name: ${this.name} <br> Task Desription: ${this.description} 
+            // <br> Assigned To: ${this.assignedTo} <br> Task Due Date: ${this.dueDate} <br> Task Status: ${this.status}<br><br>`;
+        
+            const element = document.createRange().createContextualFragment(html);
+            return element;
+        }
+    }
+
+class TaskManager {
+    constructor(tasksContainer) {
+        this.tasks =[];
+        this.currentId = 1;
+        // this.summaryContainer = summaryContainer;
+        this.tasksContainer = tasksContainer;
+    }
+
+
+    add(task) {
+        this.tasks.push(task);
+        this.display();
+
+    }
+
+    display() {
+        this.tasksContainer.innerHTML = "";
+        this.tasks.forEach(task => {
+            this.tasksContainer.append(task.toHtmlElement());
+        });
+    }
+
+    clearFields() {
+        document.querySelector('#forInputTask').value = '';
+        document.querySelector('#forTaskDescription').value = '';
+        document.querySelector('#forAssignedTo').value = '';
+        document.querySelector('#inputDate').value = '';
+        document.querySelector('#selectStatus').value = '';
+        document.getElementById('validMessage') = "";
     }
 }
 
-// java script task class object
-class TaskManager {
-    constructor() {
-        this.ID = 501;
-        this.tasks = [];
-    }
 
-    // This method is used to Display all the tasks on the console.
-    getAllTasks() {
-        //return this.tasks;
-        for (var i = 0; i < taskAdmin.tasks.length; i++) {
-            if (i === this.tasks.length) { break; }
-            console.log(this.tasks[i]);
+const task1 = new Task('9422a1', 'Task1', 'Creating Wireframes', 
+            'Jane', '05/08/2020', 'Done');
+const task2 = new Task('9422a2', 'Task2', 'Creating Classes', 
+'Robin', '25/08/2020', 'In Progress');
+
+
+
+
+document.querySelector('#task-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    let validMsg = document.getElementById('validMessage');
+    const taskName = document.querySelector('#forInputTask').value;
+    const taskDescription = document.querySelector('#forTaskDescription').value;
+    const taskDueDate = document.querySelector('#inputDate').value;
+    const taskAssignedTo = document.querySelector('#forAssignedTo').value;
+    const taskStatus = document.querySelector('#selectStatus').value;
+    
+
+     if (taskName == "" || taskDescription == "" || taskDueDate == "" || taskAssignedTo == "")
+        {
+            validMsg.innerHTML = "Please fill all the required fields.";
+        
         }
-    }
-
-    //Add task method
-    addTask(name, desc, duedate, assignedto, status) {
-        const addNewTask = new Task(this.ID++, name, desc, duedate, assignedto, status);
-        this.tasks.push(addNewTask);
-    }
-
-    //Edit method
-    UpdateTask(id, name, desc, duedate, assignedto, status) {
-        for (var i = 0; this.tasks.length; i++) {
-            if (this.tasks[i].ID === id) {
-                this.tasks[i].TaskName = name;
-                this.tasks[i].TaskDesc = desc;
-                this.tasks[i].DueDate = duedate;
-                this.tasks[i].AssignedTo = assignedto;
-                this.tasks[i].Status = status;
-                break;
-            }
+        else if (taskName.length < 8)
+        { 
+            validMsg.innerHTML = "Task name is too short";
+            console.log("name is short");
         }
-    }
-
-    //delete task method
-    deleteTask(id) {
-        this.tasks = this.tasks.filter((h) => h.ID !== id);
-        //this.tasks.pop() // pop method only deletes the last record from the array. So we cant use this.
-    }
-
-    // get task info method by status
-    getTaskInfoWithStatus(Lstatus) {
-        var strTaskInfoForStatus;
-        for (var i = 0; this.tasks.length; i++) {
-            if (i === this.tasks.length) { break; }
-            if (this.tasks[i].Status === Lstatus) {
-                strTaskInfoForStatus += "<br/>Task Id is: " + this.tasks[i].ID + " | Task name is:  " + this.tasks[i].TaskName + " | Task Description is:  " + this.tasks[i].TaskDesc + " | Due date is:  " + this.tasks[i].DueDate
-                    + " | Assigned to: " + this.tasks[i].AssignedTo + " | Status:  " + this.tasks[i].Status;
-            }
+        else if (taskDescription.length < 15) 
+        {
+            validMsg.innerHTML = "Task Description is too short";
         }
-        return strTaskInfoForStatus;
-    }
-
-    // Below methods are used to display on the html page
-    getAllTasksForHTML() {
-        var AllTasksInfo;
-        for (var i = 0; i < taskAdmin.tasks.length; i++) {
-            //if (i === this.tasks.length) { break; }
-            AllTasksInfo += ("<br />Task Id: " + this.tasks[i].ID + " | Task name:  " + this.tasks[i].TaskName + " | Task Description:  " + this.tasks[i].TaskDesc + " | Due date:  " + this.tasks[i].DueDate
-                + " | Assigned to: " + this.tasks[i].AssignedTo + " | Status:  " + this.tasks[i].Status);
+        else if (taskAssignedTo.length < 8)
+        {
+            validMsg.innerHTML = "Assigned to name is too short";
         }
-        return AllTasksInfo;
-    }
+        else
+        {
+            const task = new Task(taskManager.currentId++, taskName, taskDescription, 
+                taskAssignedTo, taskDueDate, taskStatus);
+                taskManager.add(task);
+            validMsg.innerHTML = "Task Submitted Successfully!"
+            taskManager.clearFields();
+        
+        };
+       
+});
+
+const tasksContainer = document.querySelector("#tasklist");
 
 
-} // end of Task Manager Class
-
-var taskAdmin = new TaskManager();
-//------------------------------------------- Adding tasks---------------------------------------------------
-taskAdmin.addTask("Shopping", "Buy milk and cheese", "04/08/2020", "John", "Done");
-taskAdmin.addTask("Gardening", "Water the plants", "05/08/2020", "Das", "In Progress");
-taskAdmin.addTask("Learn Html", "Submit task 1", "06/08/2020", "Mark", "Review");
-taskAdmin.addTask("Learn CSS", "Practise CSS", "07/08/2020", "Mary", "In Progress");
-taskAdmin.addTask("Learn JavaScript", "Work on sprin1 task 5", "08/08/2020", "Adam", "Done");
-taskAdmin.addTask("Learn JSON", "how to create JSON objects", "09/08/2020", "Brown", "Review");
-//---------------------------------- Display on HTML page All the Tasks----------------------------------------
-document.getElementById("DisplayAllTasksHeader").innerHTML = "Below are the 6 tasks added successfully";
-document.getElementById("AllTasks").innerHTML = taskAdmin.getAllTasksForHTML();
-//-------------------------------------- Now calling the Edit method-------------------------------------------
-taskAdmin.UpdateTask(501, "Shopping", "Buy milk and cheese", "04 / 08 / 2020", "John K Smith", "Done");
-// Display on HTML page after Editing
-document.getElementById("UpdateTasksHeader").innerHTML = "Updating Assigned to from 'John' to 'John K Smith' for the record with ID 501.";
-document.getElementById("AllTasksAfterUpdate").innerHTML = taskAdmin.getAllTasksForHTML();
-// --------------------------------------- Now calling delete method ------------------------------------------------
-taskAdmin.deleteTask(502);
-document.getElementById("DeleteTasksHeader").innerHTML = "Below are the results after deleting record with ID 502.";
-document.getElementById("AllTasksAfterDelete").innerHTML = taskAdmin.getAllTasksForHTML();
-// --------------------------------------- Now calling getTaskInfoWithStatus method ---------------------------               
-// To display in HTML
-document.getElementById("GetTasksStatusHeader").innerHTML = "Below are the results of all the records where the status is in 'Review' status.";
-document.getElementById("AllTasksWithStatus").innerHTML = taskAdmin.getTaskInfoWithStatus("Review");
+const taskManager = new TaskManager(tasksContainer);
+taskManager.add(task1);
+taskManager.add(task2);
